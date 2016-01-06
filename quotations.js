b229605c1ec29aa696f4a81328567ba51ec2123c
danieladekunle <daniel.adekunle@gmail.com>
2016-01-06 11:04:23 -0600

    Trigger resize test

--- quotations.js
+++ quotations.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 	if($(window).height() > 568) {
 		$("#menu-wrapper-child").removeClass().addClass("menu-wrapper-child-fs");
 	}
-	// window.addEventListener("resize", function() {
-	$( window ).resize(function() {
-		setTimeout(function () {
+	window.addEventListener("resize", function() {
+	// $( window ).resize(function() {
+		// setTimeout(function () {
 			// if($(window).height() > 568 && $(window).height() <= 736) {
 			if($(window).height() > 568) {
 				$("#menu-wrapper-child").removeClass().addClass("menu-wrapper-child-fs");
@@ -16,8 +16,13 @@ $(document).ready(function() {
 			else {
 				$("#menu-wrapper-child").removeClass("menu-wrapper-child-fs");
 			}
-		}, 3000);
-	// }, true);
+		// }, 500);
+	}, true);
+});
+
+$(document).ready(function() {
+	$(window).on("orientationchange",function(){
+		$(window).trigger('resize');
 	});
 });
 
