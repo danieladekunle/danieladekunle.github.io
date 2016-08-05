073135f7ed6d085c8ee04bd0ed7a01630f374d32
danieladekunle <daniel.adekunle@gmail.com>
2016-08-05 17:48:47 -0500

    Update pinterest.php

--- pinterest.php
+++ pinterest.php
@@ -1,5 +1,5 @@
 
-$data = json_decode(file_get_contents("https://api.pinterest.com/v1/pins/".trim($_GET['pinId'])."/?access_token=AXMo8Bs0USFpb-JrrFhQdDj4JyELFGgHbY5MrhVDTSgtxKBA-wAAAAA&fields=image"),true);
+$data = json_decode(file_get_contents("https://api.pinterest.com/v1/pins/".trim($_GET['pinId'])."/?access_token=TOKEN&fields=image"),true);
                                                                       
 $imgUrl = $data['data']['image']['original']['url'];
 header("Location:".$imgUrl); 
