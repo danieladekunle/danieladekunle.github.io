d0fced3a6c9cbbabcaee0876b765edc294fa2571
danieladekunle <daniel.adekunle@gmail.com>
2018-07-26 16:45:50 -0500

    Removed files, Testing reload on rotate

--- pinterest.php
+++
@@ -1,5 +0,0 @@
-
-$data = json_decode(file_get_contents("https://api.pinterest.com/v1/pins/".trim($_GET['pinId'])."/?access_token=AQBRHInu-jqty6nbBMvLIJGhky6zFGgK6hQ7iAlDTSgtxKBA-wAAAAA&fields=image"),true);
-                                                                      
-$imgUrl = $data['data']['image']['original']['url'];
-header("Location:".$imgUrl); 
